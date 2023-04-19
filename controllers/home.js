714979b14efd3156533c538ce6f884b0b9d7e9fb
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-04-19 20:26:31 +0900

    main page and ejs setting

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,18 @@
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
+const Comment = require('../models/Comment')
+const User = require('../models/User')
+
 module.exports = {
-    getIndex: (req, res) => {
-      res.render("index.ejs");
+    getIndex: async (req, res) => {
+      try {
+        const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+        res.render("index.ejs", { posts: posts });
+      } catch (err) {
+        console.log(err);
+      }
     },
+
+
   };
   
